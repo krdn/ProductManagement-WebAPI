f313d80ec2c7e72baefb18357cd826ea0536cd93
Gon Kim <krdn.net@gmail.com>
2025-05-18 08:06:11 +0900

    Program.cs 파일 추가

---
+++ Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using ProductApi.Data;
+using ProductApi.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Add DbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Register Services
+builder.Services.AddScoped<IProductService, ProductService>();
+
+// Configure Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
